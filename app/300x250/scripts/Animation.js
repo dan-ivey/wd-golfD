21e4c63062fcac3541c0defb6acc20eee41b8e43
Dan Ivey <95397173+dan-ivey@users.noreply.github.com>
2023-01-25 10:09:29 -0800

    golfD

---
+++ app/300x250/scripts/Animation.js
@@ -0,0 +1,48 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var img2 = document.getElementById('img2');
+	var logo = document.getElementById('logo');
+	var txt2 = document.getElementById('txt2');
+	var txt1 = document.getElementById('txt1');
+	var t = TweenMax;
+	var tl = new TimelineMax();
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+		
+		tl.to(img2, 1.5, {opacity:1, ease:Sine.easeInOut})
+		.to(logo, 1.5, {opacity:1, ease:Sine.easeInOut}, "-=1.5")
+		.to(txt1, 1.5, {opacity:1, ease:Sine.easeInOut}, "-=1.5")
+		.to(txt2, 1.5, {opacity:1, ease:Sine.easeInOut}, "+=1");
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
